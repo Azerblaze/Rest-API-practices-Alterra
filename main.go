c05c54703336d794da4a3569d7d4d7a52761eaeb
razerblaze <ftapamahendra@gmail.com>
2022-09-28 14:23:01 +0800

    logger middleware

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"praktikum/config"
+	m "praktikum/middleware"
 	"praktikum/route"
 )
 
 func main() {
 	config.InitDB()
 	e := route.New()
+	// implementasi middleware logger
+	m.LogMiddleware(e)
+	// start server
 	e.Logger.Fatal(e.Start(":8000"))
 }
